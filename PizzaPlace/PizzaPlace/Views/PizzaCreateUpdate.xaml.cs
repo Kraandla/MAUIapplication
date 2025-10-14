48e447b0abd531b2f9e5d6d4f950627f08468aab
corvert <orvet.priimagi@gmail.com>
2025-10-14 12:58:37 +0300

    update PizzaDetails

--- PizzaPlace/PizzaPlace/Views/PizzaCreateUpdate.xaml.cs
+++ PizzaPlace/PizzaPlace/Views/PizzaCreateUpdate.xaml.cs
@@ -1,9 +1,20 @@
+using PizzaPlace.Models;
+
 namespace PizzaPlace.Views;
 
 public partial class PizzaCreateUpdate : ContentPage
 {
-	public PizzaCreateUpdate()
+    private Pizza? pizza;
+
+    public PizzaCreateUpdate()
 	{
 		InitializeComponent();
 	}
+
+    public PizzaCreateUpdate(Pizza? pizza)
+    {
+        InitializeComponent(); // Ensure XAML is loaded
+        this.pizza = pizza;
+        BindingContext = pizza;
+    }
 }
--- PizzaPlace/PizzaPlace/Views/PizzaDetails.xaml.cs
+++ PizzaPlace/PizzaPlace/Views/PizzaDetails.xaml.cs
@@ -1,8 +1,27 @@
+using PizzaPlace.Models;
+
 namespace PizzaPlace.Views;
 
 public partial class PizzaDetails : ContentPage
 {
-	public PizzaDetails(Models.Pizza pizza)
+
+    public PizzaDetails()
+    {
+        InitializeComponent();
+
+        // Lisa testandmed (dummy info)
+        BindingContext = new Pizza
+        {
+            
+            Name = "Test Pizza",
+            Size = 18,
+            Toppings = "Cheese, Pepperoni, Olives",
+            Sauce = "Tomato Basil",
+            Price = 12.99,
+            Image = "margherita.png"
+        };
+    }
+    public PizzaDetails(Models.Pizza pizza)
 	{
 		InitializeComponent();
         this.BindingContext = pizza;
@@ -23,5 +42,31 @@ public partial class PizzaDetails : ContentPage
     {
         var pizza = BindingContext as PizzaPlace.Models.Pizza;
         await Navigation.PushModalAsync(new PizzaCreateUpdate(pizza));
+    }
+
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        var pizza = BindingContext as PizzaPlace.Models.Pizza;
+        if (pizza == null)
+        {
+            await DisplayAlert("Error", "No pizza loaded.", "OK");
+            return;
+        }
+
+        var confirm = await DisplayAlert("Delete pizza", "Are you sure you want to delete this pizza?", "Delete", "Cancel");
+        if (!confirm) return;
+
+        var db = new PizzaPlace.Data.DatabaseContext();
+        var success = await db.DeleteItemAsync<PizzaPlace.Models.Pizza>(pizza);
+
+        if (success)
+        {
+            await DisplayAlert("Deleted", "Pizza was deleted.", "OK");
+            await Navigation.PopAsync();
+        }
+        else
+        {
+            await DisplayAlert("Error", "Failed to delete pizza.", "OK");
+        }
     }
 }
