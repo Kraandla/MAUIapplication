13f8c8ab0411066814b89202cabb232839b9dbb8
Kraandla <kevinrandla22@gmail.com>
2025-10-14 13:11:39 +0300

    Back button functionality
    Button goes back to the view it came from when pressed

--- PizzaPlace/PizzaPlace/Views/PizzaCreateUpdate.xaml.cs
+++ PizzaPlace/PizzaPlace/Views/PizzaCreateUpdate.xaml.cs
@@ -6,4 +6,9 @@ public partial class PizzaCreateUpdate : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
