671f0dd921a86d3d1ab75b3afa8e305c44a6a506
katikoppel <katikoppel1995@gmail.com>
2025-10-20 13:29:46 +0300

    StartPage Image slideshow added

--- PizzaPlace/PizzaPlace/Views/StartPage.xaml.cs
+++ PizzaPlace/PizzaPlace/Views/StartPage.xaml.cs
@@ -1,14 +1,55 @@
 using Microsoft.Maui.Controls;
+using System.Collections.Generic;
 
 namespace PizzaPlace.Views;
 
 public partial class StartPage : ContentPage
 {
-    bool _animated;
+    readonly List<string> _images = new()
+    {
+        "pizza1.png",
+        "pizza2.png",
+        "pizza3.png"
+    };
+
+    int _currentIndex = 0;
+    bool _running = false;
 
     public StartPage()
     {
         InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (_running)
+            return;
+
+        _running = true;
+
+        while (_running)
+        {
+            SlideshowImage.Source = _images[_currentIndex];
+
+            //fade in
+            await SlideshowImage.FadeTo(1, 800, Easing.CubicIn);
+
+            //stay visible for a moment
+            await Task.Delay(4000);
+
+            //fade out
+            await SlideshowImage.FadeTo(0, 1000, Easing.CubicOut);
+
+            _currentIndex = (_currentIndex + 1) % _images.Count;
+        }
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _running = false;
     }
 
     async void PizzaMenu_Clicked(object sender, EventArgs e)
