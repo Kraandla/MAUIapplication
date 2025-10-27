f71e43edfa758cf12645cb94a67f18be6bfa48ec
Anna Fomina <ann.fomi77@gmail.com>
2025-10-27 14:37:16 +0300

    PizzaMenu Layout and Add pizza btn

--- PizzaPlace/PizzaPlace/Views/PizzaMenu.xaml.cs
+++ PizzaPlace/PizzaPlace/Views/PizzaMenu.xaml.cs
@@ -54,4 +54,37 @@ public partial class PizzaMenu : ContentPage
     {
         await Navigation.PushAsync(new StartPage());
     }
+
+    async void CreatePizza_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new PizzaCreateUpdate());
+    }
+
+    async void MenuButton_Clicked(object sender, EventArgs e)
+    {
+        string action = await DisplayActionSheet(
+            "Navigate to:",
+            "Cancel",
+            null,
+            "StartPage",
+            "PizzaMenu",
+            "PizzaCreate"
+        );
+
+        switch (action)
+        {
+            case "StartPage":
+                await Navigation.PushAsync(new StartPage());
+                break;
+            case "PizzaMenu":
+                await Navigation.PushAsync(new PizzaMenu());
+                break;
+            case "PizzaCreate":
+                await Navigation.PushAsync(new PizzaCreateUpdate());
+                break;
+            default:
+                // Cancel or closed, do nothing
+                break;
+        }
+    }
 }
