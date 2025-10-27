03e33ad48ca2059a940cbbfab77c61315fa02568
corvert <orvet.priimagi@gmail.com>
2025-10-27 14:13:26 +0200

    update menu

--- PizzaPlace/PizzaPlace/Views/PizzaMenu.xaml.cs
+++ PizzaPlace/PizzaPlace/Views/PizzaMenu.xaml.cs
@@ -46,12 +46,45 @@ public partial class PizzaMenu : ContentPage
 
         var selectedPizza = e.CurrentSelection.FirstOrDefault() as Pizza;
 
-        await Navigation.PushAsync(new PizzaDetails( selectedPizza));
+        await Navigation.PushAsync(new PizzaDetails(selectedPizza));
 
     }
 
     async void BackButton_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new StartPage());
+    }
+
+    async void CreatePizza_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new PizzaCreateUpdate());
+    }
+
+    async void MenuButton_Clicked(object sender, EventArgs e)
+    {
+        string action = await DisplayActionSheet(
+            "Navigate to:",
+            "Cancel",
+            null,
+            "StartPage",
+            "PizzaMenu",
+            "PizzaCreate"
+        );
+
+        switch (action)
+        {
+            case "StartPage":
+                await Navigation.PushAsync(new StartPage());
+                break;
+            case "PizzaMenu":
+                await Navigation.PushAsync(new PizzaMenu());
+                break;
+            case "PizzaCreate":
+                await Navigation.PushAsync(new PizzaCreateUpdate());
+                break;
+            default:
+                // Cancel or closed, do nothing
+                break;
+        }
     }
 }
